91617470611c0e6d9c569c6b288b6bfc143a35f6
moraud-creator <hawayabtal12@gmail.com>
2026-02-28 22:36:34 +0000

    Update verify_code.php

--- verify_code.php
+++ verify_code.php
@@ -1,17 +1,22 @@
 <?php
-// التحقق من الأكواد - نسخة محسنة
+// التحقق من الأكواد - نسخة صارمة
 $code = isset($_GET['code']) ? $_GET['code'] : '';
+
+// سجل الطلب (للتتبع)
+$log = date('Y-m-d H:i:s') . " - Code: " . $code . "\n";
+file_put_contents('log.txt', $log, FILE_APPEND);
 
 if (empty($code)) {
     echo "INVALID";
     exit;
 }
 
-// الأكواد الصالحة فقط
+// الأكواد الصالحة فقط - أضف هنا الأكواد التي يبيعها البوت
 $valid_codes = [
-    "12345678" => 1,
-    "87654321" => 1, 
-    "20081212" => 1
+    "12345678" => true,
+    "87654321" => true,
+    "20081212" => true,
+    "57298314" => true,
 ];
 
 if (isset($valid_codes[$code])) {
